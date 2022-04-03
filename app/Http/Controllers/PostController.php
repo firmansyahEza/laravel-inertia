64a584e3c60a30b66c9e161b23bfb1e7ed04ae14
RHEZA FIRMANSYAH <ezafirmansyah@hotmail.com>
2022-04-03 17:48:54 +0700

    membuat method create dan store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class PostController extends Controller
@@ -13,5 +15,27 @@ class PostController extends Controller
         return Inertia::render('Post/Index', [
             'posts' => $posts
         ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Post/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        if ($post) {
+            return Redirect::route('posts.index')->with('message', 'Data Berhasil Disimpan!');
+        }
     }
 }
