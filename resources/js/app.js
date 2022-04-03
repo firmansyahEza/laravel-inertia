6ff2856c850d6693e7fc4b97716efb5a9a281c74
RHEZA FIRMANSYAH <ezafirmansyah@hotmail.com>
2022-04-03 22:30:28 +0700

    nambah link import agar inertia-link bisa aktif

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 require("./bootstrap");
 import { createApp, h } from "vue";
-import { App, plugin } from "@inertiajs/inertia-vue3";
+import { App, plugin, Link } from "@inertiajs/inertia-vue3";
 
 const el = document.getElementById("app");
 
@@ -11,5 +11,6 @@ createApp({
             resolveComponent: (name) => require(`./pages/${name}`).default,
         }),
 })
+    .component("inertia-link", Link)
     .use(plugin)
     .mount(el);
